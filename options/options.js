5e84d553d426ebb2745652e3425b716cf4930590
Crowdbotics <team@crowdbotics.com>
2024-06-01 15:32:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "concept-terp-48217",
+  url: "https://concept-terp-48217.botics.co",
+  api: "https://concept-terp-48217.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
